2a781d549f6cc8c19326615ab04f63abdf9b288c
SuperOJ <SuperOJ2017@gmail.com>
2019-01-04 22:46:26 +0800

    二维码控件添加拖拽功能

--- userfiles/templates/dream/modules/layouts/templates/qr-code.php
+++ userfiles/templates/dream/modules/layouts/templates/qr-code.php
@@ -14,38 +14,54 @@ $database = '[{"link":"http://www.zhongyajituan.cn/","name":"中亚集团","logo
 $qrcode_settings = get_option('qrcode_settings', $params['id']);
 $qrcode_settings = $qrcode_settings ? $qrcode_settings : $database;
 ?>
+
+<!-- 拖拽 -->
+<link href="{TEMPLATE_URL}drag/dist/gridstack.css" rel="stylesheet" />
+<link href="{TEMPLATE_URL}drag/css/default.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link href="http://cdn.bootcss.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
 <style type="text/css">
-    .qricon-wrapper>img{border: 1px #5fabf3 solid;}
-    .qrcard-block>p{  font-size: 15px;color: #fff;}
-    .qrcard-block{margin-top: 10px;}
-    .qrcards-layout-1{padding-top: 35px;
-        padding-bottom: 20px;
-        height: 280px;
-        display: flex;
-        flex-direction: row;
-        text-align: center;
-        justify-content: center;}
-    .qrcards-layout-1>li{  
-        list-style: none;
-        height: 280px;
-        margin-left: 150px;}
-    .qricon-wrapper{width: 192px;}
+.grid-stack-item-content {color: #2c3e50;text-align: center;background-color: #18bc9c;}
+.qricon-wrapper>img{border: 1px #5fabf3 solid;}
+.qrcard-block>p{font-size: 15px;color: #000;}
+.qrcard-block{margin-top: 10px;}
+.qrcards-layout-1>li{list-style: none;height: 260px;}  
 </style>
 <div class="layout-qrcode-<?php print $params['id'] ?>">
-    <ul class="qrcards-layout-1">
-            <?php 
-          if(!empty($qrcode_settings)){
+    <ul class="qrcards-layout-1 grid-stack">
+
+        <?php 
+        if(!empty($qrcode_settings)){
             $qrarr = json_decode($qrcode_settings,true);
-            foreach ($qrarr as $key => $value) { ?>
-            <li class="qrcard">
-                <div class="qricon-wrapper">
-                    <img src="<?php echo $value['logo']; ?>">
-                </div>
-                <div class="qrcard-block">
-                    <p class="card-text"><?php echo $value['name']; ?></p>
+            foreach ($qrarr as $key => $value) { 
+        ?>
+
+            <li class="qrcard grid-stack-item" grid-stack-item" data-gs-x="0" data-gs-y="0" data-gs-width="2" data-gs-height="1">
+                <div class="grid-stack-item-content">
+                    <div class="qricon-wrapper">
+                        <img src="<?php echo $value['logo']; ?>">
+                    </div>
+                    <div class="qrcard-block">
+                        <p class="card-text"><?php echo $value['name']; ?></p>
+                    </div>
                 </div>
             </li>
-            <?php } 
-        } ?>
+
+        <?php 
+            } 
+        } 
+        ?>
     </ul>
 </div>
+
+<script src="{TEMPLATE_URL}drag/js/lodash.min.js"></script>
+<script src="{TEMPLATE_URL}drag/dist/gridstack.js"></script>
+<script type="text/javascript">
+$(function () {
+    var options = {
+        cell_height: 260,
+        vertical_margin: 10
+    };
+    $('.grid-stack').gridstack(options);
+});
+</script>
